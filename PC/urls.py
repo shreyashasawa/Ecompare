1518ab987d59b1fa0b46cb4993e74575fa81ef8d
shreyash <shreyashasawa25@gmail.com>
2021-05-30 14:16:57 +0530

    minor changes

--- PC/urls.py
+++ PC/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf import settings
+# from django.conf import settings
 from django.urls import include, path
 
 urlpatterns = [
