21470b3cc0fc62edde7841e07c8cbdc328fde653
shreyash <shreyashasawa25@gmail.com>
2021-06-20 14:57:32 +0530

    ♻[Unwanted statements removed]

--- PC/settings.py
+++ PC/settings.py
@@ -21,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's*$6k^smpp0y6tj-hec=*^evjpl5mq(3a8sumfvrlz2m0d)jho'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['https://ecompare-app.herokuapp.com/']
 
